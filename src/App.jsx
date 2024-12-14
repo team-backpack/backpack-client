cf32e6ff6eb9d878b3b7ea207799acc312c96058
Mateus <mateusc.ramos@outlook.com>
2024-12-14 17:49:24 -0300

    feat: update auth folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/login";
+import Login from "./pages/auth/login";
 import NotFound from "./pages/not-found";
 
 function App() {
+  const authUser = null;
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={authUser ? null : <Navigate to={"/login"} />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
