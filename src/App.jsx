f7219133f4544f5507b0bb0cb075b8b969542344
Mateus <mateusc.ramos@outlook.com>
2024-12-14 17:39:27 -0300

    feat: add login page with styling and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './pages/login'
-import NotFound from './pages/not-found'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/login";
+import NotFound from "./pages/not-found";
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,69 @@
+import { useState } from "react";
+import { FiLock } from "react-icons/fi";
+import { LuEye, LuEyeClosed, LuUser } from "react-icons/lu";
+import { Link } from "react-router-dom";
+
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container large">
+      <div className="logo">
+        <img src="src/assets/logo.png" alt="Logo backpack" />
+      </div>
+
+      <div className="divider"></div>
+
+      <div className="inputs">
+        <div className="title">
+          <h1>Olá novamente, aventureiro!</h1>
+        </div>
+
+        <form>
+          <div className="form-control">
+            <label>E-mail ou nome de usuário</label>
+            <div className="input-icons">
+              <LuUser className="icon left" />
+              <input type="text" placeholder="exemplo@email.com" />
+            </div>
+          </div>
+
+          <div className="form-control">
+            <label>Senha</label>
+            <div className="input-icons">
+              <FiLock className="icon left" />
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder={showPassword ? "123456789" : "*********"}
+              />
+              {showPassword ? (
+                <LuEyeClosed
+                  className="icon right cursor"
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              ) : (
+                <LuEye
+                  className="icon right cursor"
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              )}
+            </div>
+          </div>
+
+          <div className="form-control">
+            <button type="submit" className="blue">
+              Entrar
+            </button>
+
+            <div className="form-links">
+              <Link to={"/register"}>Criar conta</Link>
+              <Link to={"/register"}>Esqueci a senha</Link>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
